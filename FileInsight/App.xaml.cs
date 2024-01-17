4d5796807b8d1f56b07c552e11f016193026a2b0
Akn <sibata486@gmail.com>
2024-01-17 23:52:41 +0900

    init

---
+++ FileInsight/App.xaml.cs
@@ -0,0 +1,21 @@
+﻿using Prism.Ioc;
+using FileInsight.Views;
+using System.Windows;
+
+namespace FileInsight
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+        }
+    }
+}
